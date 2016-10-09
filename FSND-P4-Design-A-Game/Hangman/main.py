86e5b86bb687f785835573e196441c147dff60ad
Nakajima Kazuya <uniss010@gmail.com>
2016-10-09 14:50:11 +0900

    Corrected cron to send mail with active games

--- FSND-P4-Design-A-Game/Hangman/main.py
+++ FSND-P4-Design-A-Game/Hangman/main.py
@@ -18,26 +18,20 @@ class SendReminderEmail(webapp2.RequestHandler):
         email will be sent player who has unfinished games.
         Called every day using a cron job"""
         app_id = app_identity.get_application_id()
-        distribution = []
         users = User.query(User.email != None).fetch()
         games = Game.query().fetch()
 
         for user in users:
-            for game in games:
-                if (user.name == game.user_name) and (game.game_over == False):
-                    distribution.append(user)
-                    return
-
-        for user in distribution:
-            subject = 'This is a reminder!'
-            body = 'Hello {}, try out Hangman!'.format(user.name)
-            # This will send test emails, the arguments to send_mail are:
-            # from, to, subject, body
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-                           user.email,
-                           subject,
-                           body)
-
+            games = Game.query(Game.user == user.key, Game.game_over == False).fetch
+            if games:
+                subject = 'This is a reminder!'
+                body = 'Hello {}, try out Hangman!'.format(user.name)
+                # This will send test emails, the arguments to send_mail are:
+                # from, to, subject, body
+                mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+                               user.email,
+                               subject,
+                               body)
 
 class UpdateAverageMovesRemaining(webapp2.RequestHandler):
     def post(self):
